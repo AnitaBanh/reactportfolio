5050568bdb922d16b56088639299b7341f5c5574
Anita Banh <120350675+AnitaBanh@users.noreply.github.com>
2023-03-29 11:17:01 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -9,8 +9,11 @@ export default function About() {
       <Image src={avatar} rounded/>
       <h1>About me</h1>
       <p>
-      Molecular Biologist with background in medical device quality assurance and full-stack web development. Effective at cross-functional communication. Known for strong attention to detail and clear documentation.
-      </p>
+Welcome to my personal portfolio page! My name is Anita and I'm a software developer with a passion for creating robust, user-friendly applications. I recently completed an intense certificate program at the University of Texas at Austin, where I learned full stack coding using the MERN stack.
+
+Prior to my career transition, I spent over 15 years as a molecular biologist and in medical device quality assurance. During that time, I honed my skills in cross-functional communication, paying close attention to detail, and creating clear documentation. These skills have been invaluable in my software development work, where I strive to deliver high-quality code that meets the needs of end-users.
+
+When I'm not coding, I enjoy exploring new cuisines (hold the cilantro) and caring for my house plants. Thanks for visiting my portfolio page and feel free to get in touch with any questions or opportunities!      </p>
     </div>
   );
 }
